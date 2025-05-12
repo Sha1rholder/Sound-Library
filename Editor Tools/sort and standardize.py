032473508b60bae7ef86afc2b0e96143753ab108
Sha1rholder <sha1rholder@outlook.com>
2025-05-12 09:29:31 +0800

    update data

--- Editor Tools/sort and standardize.py
+++ Editor Tools/sort and standardize.py
@@ -1,12 +1,7 @@
 import pandas as pd
 
-paths = [
-    "./data/over-ear score cn.csv",
-    "./data/over-ear sensitivity official.csv",
-]
-
-for path in paths:
-    sort = pd.read_csv(path, dtype=str)
-    sort.sort_values(by=["brand", "model"], ascending=[
-                     True, True], inplace=True)
-    sort.to_csv(path, index=False)
+path = "./data/over-ear sensitivity official.csv"
+sort = pd.read_csv(path, dtype=str)
+sort.sort_values(by=["brand", "model"], ascending=[
+    True, True], inplace=True)
+sort.to_csv(path, index=False)
